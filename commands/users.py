6d6ffe0f091acdc47bf2e0fc4882ca8bfb3671f2
XorV2 <reubenmcbride60@gmail.com>
2022-05-29 01:39:33 +0100

    updates

--- commands/users.py
+++ commands/users.py
@@ -1,2 +1,9 @@
-def users_command():
-    ...
+def users_command(client, functions: dict[str, object]):
+    open_file: object = functions["open_file"]
+    users = open_file("db", "users.json")
+    to_output = ""
+
+    for each_user in users:
+        to_output += f"  {each_user}\n"
+
+    client.send(to_output.encode())
