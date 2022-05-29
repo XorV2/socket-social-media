d88bce4edacf565fde0134e331fe0feddb010b05
XorV2 <reubenmcbride60@gmail.com>
2022-05-29 10:18:18 +0100

    updates

--- commands/view.py
+++ commands/view.py
@@ -1,13 +1,29 @@
+def _check(command):
+    """
+    this functions purpose is to check that the command can be split with :
+    if it can, return [True, split_command], if not, return [False]
+    """
+    try:
+        split_command = command.split(":")
+    except:
+        return [False]
+    else:
+        return [True, split_command]
+
+
 def view_command(client, command, open_file):
     users = open_file("db", "users.json")
 
-    try:
-        user_to_view = command.split(":")[1]
-    except:
+    split_command = _check(command)
+    if not split_command[0]:
         client.send(b"Please use this format, view:username")
-    else:
-        if user_to_view in users:
-            info_about_viewed_user = users[user_to_view]
-            client.send(info_about_viewed_user.encode())
-            return None
+        return None
+
+    user_to_view = split_command[1]
+
+    if user_to_view not in users:
         client.send(f"User {user_to_view} does not exist.".encode())
+        return None
+
+    info_about_user = users["username"]
+    client.send(info_about_user.encode())
